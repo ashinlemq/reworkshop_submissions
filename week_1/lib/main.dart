4572fa334c2fb8a1eebdf06b652d52b5261c1822
ashinlemq <ashinlemist.rahman@students.mq.edu.au>
2026-06-30 22:52:39 +1000

    Implement week_1 screen4

--- week_1/lib/main.dart
+++ week_1/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:week_1/screens/screen1.dart';
 import 'package:week_1/screens/screen2.dart';
 import 'package:week_1/screens/screen3.dart';
+import 'package:week_1/screens/screen4.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Screen3(), // Select screens 1-6
+      home: Screen4(), // Select screens 1-6
     );
   }
 }
---
+++ week_1/lib/screens/screen4.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Screen4 extends StatelessWidget {
+  const Screen4 ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.teal,
+      body: Center(
+        child: Text("BingeFlix"),
+      ),
+    );
+  }
+}
