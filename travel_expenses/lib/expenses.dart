462002f8ce8bc14e852816da14903e050725ccf1
ashinlemq <ashinlemist.rahman@students.mq.edu.au>
2026-07-04 01:37:47 +0600

    week_5 replace placeholders with hardcoded data

--- travel_expenses/lib/expenses.dart
+++ travel_expenses/lib/expenses.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:travel_expenses/widgets/expenses_list.dart';
+import 'models/expense_model.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -7,10 +9,24 @@ class Expenses extends StatefulWidget {
   State<StatefulWidget> createState() {
     return _ExpensesState();
   }
-
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _myExpenses = [
+    Expense(
+      name: 'Birthday Dinner',
+      amount: 300.0,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      name: 'Kayaking tour',
+      amount: 70.0,
+      date: DateTime.now(),
+      category: Category.experience,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,9 +34,7 @@ class _ExpensesState extends State<Expenses> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text("Travel Expenses"),
-            Text("Chart Placeholder"),
-            Text("Expenses List Placeholder"),
+            Expanded(child: ExpensesList(allExpenses: _myExpenses)),
           ],
         ),
       ),
